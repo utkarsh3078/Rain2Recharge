45e3a2a7808ac2bdd65108907ed0e8b465455e66
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2025-09-21 17:48:05 +0530

    small update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Routes from "./Routes";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
